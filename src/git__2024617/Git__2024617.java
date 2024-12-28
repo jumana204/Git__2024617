86ce740c2b6e959534f4c62a6f6b399a0e45acb7
Jumana <2024617@student.cct.ie>
2024-12-28 18:00:39 +0000

    Initial commit

---
+++ src/git__2024617/Git__2024617.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git__2024617;
+
+/**
+ *
+ * @author juman
+ */
+public class Git__2024617 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
